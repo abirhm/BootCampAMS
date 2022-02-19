0449382f4b35502612c6dd41b6e5e90869020d45
abirhm <abir.hammami@sesame.com.tn>
2022-02-19 20:34:33 +0100

    first commit

---
+++ src/main/java/com/sip/ams/entities/Provider.java
@@ -0,0 +1,67 @@
+
+package com.sip.ams.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Provider {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private long id;
+
+@NotBlank(message = "Name is mandatory") 
+@Column(name ="name")
+String name;
+
+@NotBlank(message = "Address  is mandatory") 
+@Column(name="address")
+private String address;
+
+@NotBlank(message = "Email  is mandatory") 
+@Column(name="email")
+private String email;
+//constructeur with filds
+public Provider(long id, String name, String address, String email) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.address = address;
+	this.email = email;
+}
+//constructeur with out filds
+public Provider() {
+	super();
+}
+//get+set
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+
+}
---
+++ src/main/java/com/sip/ams/repositories/ProviderRepository.java
@@ -0,0 +1,9 @@
+package com.sip.ams.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sip.ams.entities.Provider;
+
+public interface ProviderRepository extends CrudRepository<Provider, Long>  {
+
+}
